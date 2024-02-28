e92a019dad2110424dde96597587ac4b8356e948
Mike Romain <mchlrmn@me.com>
2024-02-28 18:09:10 +0100

    Add logo and change title

--- src/TournamentCard.tsx
+++ src/TournamentCard.tsx
@@ -3,6 +3,7 @@ import {
   Box,
   HStack,
   Image,
+  Link,
   Skeleton,
   Stack,
   Text,
@@ -18,62 +19,66 @@ interface Props {
 
 export const TournamentCard: React.FC<Props> = ({ tournament }) => {
   return (
-    <Stack
-      spacing={{ base: "3", md: "5" }}
-      borderRadius="lg"
-      shadow="md"
-      borderWidth="1px"
-      overflow="hidden"
-      cursor="pointer"
-      _hover={{
-        transform: "scale(1.01)",
-        shadow: "lg",
-        transition: "0.2s ease-in-out",
-      }}
-    >
-      <Box position="relative">
-        <AspectRatio ratio={16 / 9}>
-          <Image
-            src={tournament.images[0]?.url ?? "https://via.placeholder.com/200"}
-            alt={tournament.name}
-            draggable="false"
-            fallback={<Skeleton />}
-          />
-        </AspectRatio>
-      </Box>
-      <Stack p={4}>
-        <Stack spacing="2">
-          <HStack spacing="1">
-            <FiMapPin />
-            <Text
-              fontSize="sm"
-              color={useColorModeValue("gray.600", "gray.300")}
-            >
-              {tournament.venueName}
-            </Text>
-          </HStack>
-          <HStack spacing="1">
-            <FiClock />
-            <Text
-              fontSize="sm"
-              color={useColorModeValue("gray.600", "gray.300")}
-            >
-              {dayjs(tournament.startAt * 1000).format("DD/MM")} -{" "}
-              {dayjs(tournament.endAt * 1000).format("DD/MM, YYYY")}
-            </Text>
-          </HStack>
-          <HStack spacing="1">
-            <FiUser />
-            <Text
-              fontSize="sm"
-              color={useColorModeValue("gray.600", "gray.300")}
-            >
-              {tournament.numAttendees}
-            </Text>
-          </HStack>
-          <Text fontWeight="medium">{tournament.name}</Text>
+    <Link href={`https://start.gg${tournament.url}`} isExternal>
+      <Stack
+        spacing={{ base: "3", md: "5" }}
+        borderRadius="lg"
+        shadow="md"
+        borderWidth="1px"
+        overflow="hidden"
+        cursor="pointer"
+        _hover={{
+          transform: "scale(1.01)",
+          shadow: "lg",
+          transition: "0.2s ease-in-out",
+        }}
+      >
+        <Box position="relative">
+          <AspectRatio ratio={16 / 9}>
+            <Image
+              src={
+                tournament.images[0]?.url ?? "https://via.placeholder.com/200"
+              }
+              alt={tournament.name}
+              draggable="false"
+              fallback={<Skeleton />}
+            />
+          </AspectRatio>
+        </Box>
+        <Stack p={4}>
+          <Stack spacing="2">
+            <HStack spacing="1">
+              <FiMapPin />
+              <Text
+                fontSize="sm"
+                color={useColorModeValue("gray.600", "gray.300")}
+              >
+                {tournament.venueName}
+              </Text>
+            </HStack>
+            <HStack spacing="1">
+              <FiClock />
+              <Text
+                fontSize="sm"
+                color={useColorModeValue("gray.600", "gray.300")}
+              >
+                {dayjs(tournament.startAt * 1000).format("DD/MM")} -{" "}
+                {dayjs(tournament.endAt * 1000).format("DD/MM, YYYY")}
+              </Text>
+            </HStack>
+            <HStack spacing="1">
+              <FiUser />
+              <Text
+                fontSize="sm"
+                color={useColorModeValue("gray.600", "gray.300")}
+              >
+                {tournament.numAttendees}
+              </Text>
+            </HStack>
+            <Text fontWeight="medium">{tournament.name}</Text>
+          </Stack>
         </Stack>
       </Stack>
-    </Stack>
+    </Link>
   );
 };
